e42851a58f0106ceee56606263bebd0c49bbfd72
nadirbasalamah <nadir008basalamah@gmail.com>
2024-02-13 12:46:45 +0700

    feat(app): initialize repository and add core features

---
+++ src/components/home/BooksCard.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+import BookSingleCard from "./BookSingleCard";
+
+const BooksCard = ({ books }) => {
+  return (
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {books.map((book) => (
+        <BookSingleCard key={book._id} book={book} />
+      ))}
+    </div>
+  );
+};
+
+BooksCard.propTypes = {
+  books: PropTypes.array,
+};
+
+export default BooksCard;
